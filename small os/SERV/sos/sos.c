a75eddcdc7c64c56b48a852e742e5433e906c747
Mahmoud Matarawy <30804086+MahmoudMatarawy@users.noreply.github.com>
2023-06-01 17:23:36 +0300

    Update sos.c
    add create and delete tasks functions

--- small os/SERV/sos/sos.c
+++ small os/SERV/sos/sos.c
@@ -83,3 +83,49 @@ enu_system_status_t SOS_deinit (void)
 	}
 	return enu_system_status_retVal;
 }
+
+enu_system_status_t SOS_create_task(ptr_function_name_t ptr_function_name , uint8_t u8_task_id , uint8_t u8_task_periority,uint16_t u16_task_period)
+{
+	enu_system_status_t enu_system_status_retVal = SOS_STATUS_SUCCESS;
+	if ((NULL != ptr_function_name) && (INVALID_TASK_ID > u8_task_id) && (INVALID_TASK_PERIORITY > u8_task_periority)
+	&& (INVALID_TASK_PERIOD > u16_task_period) && (NULL == arr_st_gs_task_config[u8_gs_max_count_of_tasks].ptr_function_name)
+	&& (SOS_STATUS_INVALID_STATE != SOS_token_config_param(ptr_function_name,u8_task_id,u8_task_periority)))
+	{
+		
+		arr_st_gs_task_config[u8_gs_max_count_of_tasks].ptr_function_name = ptr_function_name;
+		arr_st_gs_task_config[u8_gs_max_count_of_tasks].u8_task_periority = u8_task_periority;
+		arr_st_gs_task_config[u8_gs_max_count_of_tasks].u16_task_period = u16_task_period;
+		arr_st_gs_task_config[u8_gs_max_count_of_tasks].u16_remain_time = u16_task_period;
+		arr_st_gs_task_config[u8_gs_max_count_of_tasks].u8_task_id = u8_task_id;
+		u8_gs_arr_index_periority[u8_task_periority]= u8_gs_max_count_of_tasks;
+		u8_gs_arr_index_id[u8_task_id] = u8_gs_max_count_of_tasks;
+		u8_gs_max_count_of_tasks++;
+	}
+	else
+	{
+		enu_system_status_retVal = SOS_STATUS_INVALID_STATE;
+	}
+	return enu_system_status_retVal;
+}
+enu_system_status_t SOS_delete_task(uint8_t u8_task_id)
+{
+	enu_system_status_t enu_system_status_retVal = SOS_STATUS_SUCCESS;
+	if ((INIT == u8_gs_sos_module_state) && (NOT_INIT != u8_gs_arr_index_id[u8_task_id]))
+	{
+		u8_gs_arr_index_periority[arr_st_gs_task_config[u8_gs_arr_index_id[u8_task_id]].u8_task_periority]=NOT_INIT;
+		arr_st_gs_task_config[u8_gs_arr_index_id[u8_task_id]].ptr_function_name = NULL;
+		arr_st_gs_task_config[u8_gs_arr_index_id[u8_task_id]].u8_task_periority = NOT_INIT;
+		arr_st_gs_task_config[u8_gs_arr_index_id[u8_task_id]].u16_task_period = NOT_INIT;
+		arr_st_gs_task_config[u8_gs_arr_index_id[u8_task_id]].u8_task_id = NOT_INIT;
+		u8_gs_arr_index_id[u8_task_id] = NOT_INIT;
+		
+		SOS_rearrange_tasks();
+		
+		u8_gs_max_count_of_tasks--;
+	}
+	else
+	{
+		enu_system_status_retVal = SOS_STATUS_INVALID_STATE;
+	}
+	return enu_system_status_retVal;
+}
